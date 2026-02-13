3f5b442106922d9e00d1166b9c6f791265f4c143
Shaivi Sahu <sahushaivi00@gmail.com>
2026-02-13 23:28:04 +0530

    initial code

---
+++ userservice/userservice/src/main/java/com/fitness/userservice/model/user.java
@@ -0,0 +1,33 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable) = false
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDataTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}
