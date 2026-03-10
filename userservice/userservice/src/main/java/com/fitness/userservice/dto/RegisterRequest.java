17fc69580d37c1c5f395e485800e50653d5e38ed
Shaivi Sahu <sahushaivi00@gmail.com>
2026-03-10 13:49:06 +0530

    update

--- userservice/userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
+++ userservice/userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.fitness.userservice.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
