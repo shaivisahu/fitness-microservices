9b8a95b341ca9e7d9ae812b1e015577616810718
Shaivi Sahu <sahushaivi00@gmail.com>
2026-03-30 01:08:20 +0530

    feat: setup centralized config server and service discovery using Eureka

--- fittrack-microservices/fittrack-ms/eureka-server/src/main/java/com/fittrack/eureka/EurekaServerApplication.java
+++ fittrack-microservices/fittrack-ms/eureka-server/src/main/java/com/fittrack/eureka/EurekaServerApplication.java
@@ -1,6 +1,7 @@
 package com.fittrack.eureka;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
